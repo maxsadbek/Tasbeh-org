552674ac950be2f890ba7d648401308097221f82
maxsad_bek <masadanb@gmail.com>
2025-08-21 15:05:57 +0500

    add

--- script.js
+++ script.js
@@ -2,27 +2,28 @@
 let decrementBtn = document.querySelector("#decrementBtn");
 let incrementBtn = document.querySelector("#incrementBtn");
 let p = document.querySelector("#p");
-let SaveBtn = document.querySelector("#saveStep")
-let SaveEntiresDiv = document.querySelector("#SaveEntires")
 let resetBtn = document.querySelector("#reset")
+let selectBtn = document.querySelector("#select")
 let count = 0;
-let arr = []
+
 function decrementStep() {
-    count--;
-    p.textContent = count;
+  count--;
+  p.textContent = count;
 }
+
 
 function incrementStep() {
-    count++;
-    p.textContent = count;
-}
-  
-function resetFnc(){
-    count = 0
-    p.textContent =count
+  count++;
+  p.textContent = count;
 }
 
+function resetFnc() {
+  let alrt = alert("Confirm Reset:\nYou're about to reset the counter to 0")
+  count = 0
+  p.textContent = count
+}
+
+
 resetBtn.addEventListener("click", resetFnc)
-SaveBtn.addEventListener("click", saveStep)
 incrementBtn.addEventListener("click", incrementStep);
 decrementBtn.addEventListener("click", decrementStep);
